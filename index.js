1d9425f585bdff19e1f83d0d713361a1d90184ca
Jonathan Moore <jmjonemoore@gmail.com>
2019-05-03 14:45:32 -0500

    Final Commit

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
 'use strict';
-
-// 'STORE' is responsible for storing the underlying data
-//  that our app needs to keep track of in order to work.
-// for a shopping list, our data model is pretty simple.
-// we just have an array of shopping list items. each one
-// is an object with a `name` and a `checked` property that
-// indicates if it's checked off or not.
-// we're pre-adding items to the list so there's something
-// to see when the page first loads.
 const STORE = [
   {id: cuid(), name: 'apples', checked: false},
   {id: cuid(), name: 'oranges', checked: false},
@@ -39,49 +30,25 @@ function generateShoppingItemsString(shoppingList){
 }
 
 function renderShoppingList(){
-  // this function will be responsible for rendering the shopping list in
-  // the DOM
-
-  // for each item in STORE, generate a string representing an <li> with:
-  //    *the item name rendered as inner text
   const shoppingListItemsString = generateShoppingItemsString(STORE);
   $('.js-shopping-list').html(shoppingListItemsString);
-  //    *the item's index in the STORE set as a data attribute on the <li>
-  //    *the item's checked state (true or false) rendered as the presnece
-  //     or absence of a CSS class for indicating checked items (specifically,
-  //     .shopping-item__checked from index.css)
-  // join together the individual item strings into one long string
-  // insert the <li>s string inside the .js-shopping-list <ul> in the DOM
-  console.log('`renderShoppingList` ran');
 }
 
 function addItemToShoppingList(itemName){
-  console.log(`Adding '${itemName}' to shopping list`);
   STORE.push({id: cuid(), name: itemName, checked: false});
 }
 
 function handleNewItemSubmit(){
-  // this function will be responsible for when users add a new shopping
-  // list item
 
   $('#js-shopping-list-form').submit(function(e){
     e.preventDefault();
     const newItemName = $('.js-shopping-list-entry').val();
-    console.log(newItemName);
     $('.js-shopping-list-entry').val('');
-    // listen for a click on the submit button.
-    // prevent default behavior.
-    // capture the value that's entered in the form
-    // convert that value into an object
     addItemToShoppingList(newItemName);
-    // push that object into the STORE array
-    renderShoppingList();
-    // call renderShoppingList()
-    console.log('`handleNewItemSubmit` ran');});
+    renderShoppingList();});
 }
 
 function toggleCheckedForListItem(itemId){
-  console.log('Toggling checked property for item with id' + itemId);
   const item = STORE.find(item => item.id === itemId);
   item.checked = !item.checked;
 }
@@ -92,46 +59,25 @@ function getItemIDFromElement(item){
 }
 
 function handleItemCheckClicked(){
-  // this function will be responsible for when users click the "check" button
-  // on a shopping list item.
   $('.js-shopping-list').on('click','.js-item-toggle', event => {
-    console.log('`handleItemCheckClicked` ran');
     const id = getItemIDFromElement(event.currentTarget);
     toggleCheckedForListItem(id);
     renderShoppingList();
   });
-  // listen for a click on the check button in the item list.
-  // when clicked, the check value for the object in the STORE switches (e.g.
-  // if the object in STORE has the value of 'true' at checked, change it to false,
-  // etc.)
-  // call renderShoppingList()
 }
 
 function deleteItem(itemId){
-  console.log('Delete item with id' + itemId);
   const itemIndex = STORE.findIndex(item => item.id === itemId);
   STORE.splice(itemIndex, 1);
 }
 
 function handleDeleteItemClicked(){
-  // this function will be responsible for when users want to delete
-  // a shopping list item
   $('.js-shopping-list').on('click','.js-item-delete', event => {
-    console.log('`handleDeleteItemClicked` ran');
     const id = getItemIDFromElement(event.currentTarget);
     deleteItem(id);
     renderShoppingList();
   });
-  // listen for a click on the delete button in the item list
-  // when clicked, it removes the corresponding object from STORE
-  // call renderShoppingList()
-  console.log('`handleDeleteItemClicked` ran');
 }
-
-// this function will be our callback when the page loads. It's responsible
-// for  initially rendering the shopping list, and activating our individual
-// functions that handle new item submissions and user clicks on the "check"
-// and "delete" buttons for individual shopping list items.
 
 function handleShoppingList(){
   renderShoppingList();
